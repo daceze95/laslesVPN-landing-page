cddee98bb70d5875f14aa85b8f8c56373a4b590d
Darlington Arinze <darlingtonarinze1995@gmail.com>
2023-05-20 07:08:59 +0100

    fix path error

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavBar from '../components/NavBar.jsx';
 import Hero from '../components/Hero.jsx';
 import ChoosePlan from '../components/ChoosePlan.jsx';
-import ServersView from '../components/serversView.jsx';
+import ServersView from '../components/ServersView.jsx';
 import Carousel from '../components/Carousel.jsx';
 import Footer from '../components/Footer.jsx';
 
